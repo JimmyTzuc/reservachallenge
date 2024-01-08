a6d4aebc6408d57cf01ea645babc85f0da4bd1b0
Jimmy Tzuc <stjimmytzuc@gmail.com>
2024-01-07 23:31:53 -0600

    chore: add temporary key in db

--- core/common/management/commands/populatedb.py
+++ core/common/management/commands/populatedb.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import get_user_model
 from django.core.management.base import BaseCommand
+from apicity.models import Key
 
 
 class Command(BaseCommand):
@@ -13,6 +14,12 @@ class Command(BaseCommand):
             "admin@admin.com",
             "admin",
             **{"is_active": True, "is_staff": True, "is_superuser": True}
+        )
+
+        Key.objects.create(
+            name_service="weather",
+            public_key="45b95443f43ab6762c4b4b153d9e261f",
+            private_key=""
         )
 
     def handle(self, *args, **options):
