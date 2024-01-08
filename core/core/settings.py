0302c5a862f952e3d0c9d69f453b040efe903358
Jimmy Tzuc <stjimmytzuc@gmail.com>
2024-01-08 11:56:46 -0600

    chore: add allowed host ec2

--- core/core/settings.py
+++ core/core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY", "foobar")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["0.0.0.0", "localhost"]
+ALLOWED_HOSTS = ["0.0.0.0", "localhost", "18.217.55.215"]
 
 
 # Application definition
